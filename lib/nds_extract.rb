e611e74d051c92906561807cfd9bd9f5eb8ea061
tom hannaford <thom.hannaford@gmail.com>
2020-01-25 22:22:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,7 +55,8 @@ def gross_per_studio(collection)
     column_index = 0 
     while column_index < collection[row_index][:movies].length do 
       if studio_grosses.key?(collection[row_index][:movies][column_index][:studio])
-        studio_grosses[collection[row_index][:movies][column_index][:studio]] =
+        studio_grosses[collection[row_index][:movies][column_index][:studio]] = collection[row_index][:movies][column_index][:worldwide_gross]
+        elsif 
 end
 
 def movies_with_directors_set(source)
